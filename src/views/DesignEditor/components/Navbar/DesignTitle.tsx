c050e6f9ab914a321e828b282665233711b2c6bd
Jose Carlos <josecarlosgironsolano@gmail.com>
2025-05-16 12:40:23 -0400

    feat(Guardado): añadir funcionalidad para mostrar si hay cambios guardados

--- src/views/DesignEditor/components/Navbar/DesignTitle.tsx
+++ src/views/DesignEditor/components/Navbar/DesignTitle.tsx
@@ -1,17 +1,25 @@
 import React from "react"
 import { Input } from "baseui/input"
 import { Block } from "baseui/block"
-import CloudCheck from "~/components/Icons/CloudCheck"
+import Cloud from "~/components/Icons/Cloud"
 import { StatefulTooltip } from "baseui/tooltip"
 import useDesignEditorContext from "~/hooks/useDesignEditorContext"
+import { Check } from "lucide-react"
+import { useEditor } from "@layerhub-io/react"
+import { TriangleAlert } from "lucide-react"
 
 interface State {
   name: string
   width: number
 }
 
-const DesignTitle = () => {
+type Props = {
+  changesSaved: boolean;
+};
+
+const DesignTitle = ({ changesSaved }: Props) => {
   const [state, setState] = React.useState<State>({ name: "My first design.", width: 0 })
+  const editor = useEditor()
   const { currentDesign, setCurrentDesign } = useDesignEditorContext()
   const inputTitleRef = React.useRef<Input>(null)
   const spanRef = React.useRef<HTMLDivElement | null>(null)
@@ -22,7 +30,9 @@ const DesignTitle = () => {
   }
 
   React.useEffect(() => {
-    const name = currentDesign.name
+    const key = sessionStorage.getItem("project_key")
+    const currentProject = sessionStorage.getItem(key!)
+    const name = currentProject ? JSON.parse(currentProject).name : currentDesign.name
     if (name || name === "") {
       spanRef.current!.innerHTML = name
       setState({ ...state, name: name, width: spanRef.current?.clientWidth! + 20 })
@@ -103,7 +113,13 @@ const DesignTitle = () => {
             },
           },
         }}
-        content={() => <Block backgroundColor="#ffffff">All changes are saved</Block>}
+        content={
+          changesSaved ? (
+            <Block backgroundColor="#ffffff">All changes are saved</Block>
+          ) : (
+            <Block backgroundColor="#ffffff">Changes have not been saved</Block>
+          )
+        }
       >
         <Block
           $style={{
@@ -111,9 +127,24 @@ const DesignTitle = () => {
             padding: "10px",
             display: "flex",
             color: "#ffffff",
+            position: "relative",
           }}
         >
-          <CloudCheck size={24} />
+          <Cloud size={30} />
+          <Block
+            $style={{
+              position: "absolute",
+              left: "50%",
+              top: "52%",
+              transform: "translate(-50%, -50%)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              pointerEvents: "none",
+            }}
+          >
+            {changesSaved ? (<Check size={14} color="#000000" strokeWidth={4} />) : <TriangleAlert size={13} color="#000000" strokeWidth={4} />}
+          </Block>
         </Block>
       </StatefulTooltip>
     </Block>
