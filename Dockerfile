6f9434fe0ed9bed66a33a1b3029802afb54e08e1
Jose Carlos <josecarlosgironsolano@gmail.com>
2025-06-17 12:29:21 -0400

    feat(Docker): añadir configuraciones para docker

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,20 @@ FROM node:18-alpine as build
 # set working directory
 WORKDIR /app
 
-ADD . .
+# copy files
+COPY . .
 
-RUN npm i -g pnpm
-RUN pnpm i
+# install dependencies and build using npm
+RUN npm install
+RUN npm run build
 
-RUN pnpm build
-
+# production stage
 FROM nginx:1.19-alpine
 
-COPY nginx/templates /etc/nginx/templates/
 
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+# copy built files from build stage
 COPY --from=build /app/dist /usr/share/nginx/html
 
+# start nginx
 CMD ["nginx", "-g", "daemon off;"]
--- src/constants/environment/environment.ts
+++ src/constants/environment/environment.ts
@@ -3,10 +3,11 @@
 export const environment = {
   production: false,
   //apiBaseUrl: "https://66sqhsvx-8080.brs.devtunnels.ms/api",
-  //apiBaseUrl: "http://localhost:8088/api",
-  apiBaseUrl: "https://www.mireserva.miclases.com/api",
+  apiBaseUrl: "http://host.docker.internal:8088/api",
+  //apiBaseUrl: "https://www.mireserva.miclases.com/api",
   authServiceUrl: "https://auth.example.com",
   analyticsServiceUrl: "https://analytics.example.com",
   //frontUrl: "https://66sqhsvx-4200.brs.devtunnels.ms/",
-  frontUrl: "https://www.mireserva.miclases.com/",
+  //frontUrl: "https://www.mireserva.miclases.com/",
+  frontUrl: "http://host.docker.internal:4200/",
 }
