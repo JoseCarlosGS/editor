476b7ee46066f065967959ca8696365dc9e174fe
Jose Carlos <josecarlosgironsolano@gmail.com>
2025-07-18 09:31:11 -0400

    refactor(Dominio): cambiar urls al nuevo dominio

--- src/constants/environment/environment.ts
+++ src/constants/environment/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
   production: false,
   //apiBaseUrl: "https://66sqhsvx-8080.brs.devtunnels.ms/api",
   //apiBaseUrl: "http://localhost:8088/api",
-  apiBaseUrl: "https://www.mireserva.miclases.com/api",
+  apiBaseUrl: "https://www.even10x.com/api",
   authServiceUrl: "https://auth.example.com",
   analyticsServiceUrl: "https://analytics.example.com",
   //frontUrl: "https://66sqhsvx-4200.brs.devtunnels.ms/",
-  frontUrl: "https://www.mireserva.miclases.com/",
+  frontUrl: "https://www.even10x.com.com/",
 }
